7f408367b951795870bb73f3c9264e144c3caa87
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-03 23:50:17 +1000

    Add DebugPatches class with Debugger.LogInvalidTween patch.

---
+++ Moonlighter/DebugPatches.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening.Core;
+using HarmonyLib;
+
+namespace Moonlighter;
+
+[HarmonyPatch]
+public static class DebugPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(Debugger), nameof(Debugger.LogInvalidTween))]
+    public static bool Debugger_LogInvalidTween()
+    {
+        return false;
+    }
+}

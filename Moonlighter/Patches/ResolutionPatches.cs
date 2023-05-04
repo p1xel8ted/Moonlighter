3a0c9610dc2088c4d6d6b931e83d49d70450bb22
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-04 21:43:43 +1000

    Add resolution options to OptionsGraphicTabContent_CheckResolutions.

---
+++ Moonlighter/Patches/ResolutionPatches.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Moonlighter;
+
+[HarmonyPatch]
+public static class ResolutionPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(OptionsGraphicTabContent), nameof(OptionsGraphicTabContent.CheckResolutions))]
+    public static bool OptionsGraphicTabContent_CheckResolutions(ref OptionsGraphicTabContent __instance)
+    {
+        var maxRefresh = Screen.resolutions.Max(a => a.refreshRate).refreshRate;
+        var nativeResolution = new Resolution
+        {
+            width = Display.main.systemWidth,
+            height = Display.main.systemHeight,
+            refreshRate = maxRefresh
+        };
+
+        var newResList = Enumerable.ToList(Screen.resolutions);
+        newResList.Add(nativeResolution);
+
+        var newArList = Enumerable.ToList(newResList.Select(res => (float) res.width / res.height));
+
+        __instance._availableResolutions = newResList;
+        __instance._availableAR = newArList;
+
+        return false;
+    }
+}

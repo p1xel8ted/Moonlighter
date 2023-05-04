84b82be8b1dd72a4b595c939bd9d3fbb45b912b1
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-04 21:43:27 +1000

    Add SkipPatches.cs with Harmony patches for VideoSequence and MainMenuPanel.

---
+++ Moonlighter/Patches/SkipPatches.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+
+namespace Moonlighter;
+
+[HarmonyPatch]
+public static class SkipPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(VideoSequence), nameof(VideoSequence.Play))]
+    public static bool VideoSequence_Play(ref VideoSequence __instance)
+    {
+        return false;
+    }
+
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.ShowGamepadGUI))]
+    public static bool MainMenuPanel_ShowGamepadGUI(ref MainMenuPanel __instance)
+    {
+        __instance.CloseGamepadPanel();
+        return false;
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.PlayPressKeyReminderAnimationDelayed))]
+    public static void MainMenuPanel_PlayPressKeyReminderAnimationDelayed(ref MainMenuPanel __instance)
+    {
+        __instance.PlayOpenAnimation();
+        __instance.StopPressKeyReminderAnimation();
+        __instance.SetInteractable(true, __instance.mainMenuButtons);
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.CheckContinueAndSelectFirstButton))]
+    public static void MainMenuPanel_CheckContinueAndSelectFirstButton(ref MainMenuPanel __instance)
+    {
+        if (__instance.HasLoadedGame)
+        {
+            __instance.StartGame();
+        }
+    }  
+}

84c7b36c59553adcec892333b2dca4d8db2fdc24
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-06 18:33:37 +1000

    Refactor intro and loading screen skipping in MainMenuPanel and VideoSequence.

--- Moonlighter/Patches/SkipPatches.cs
+++ Moonlighter/Patches/SkipPatches.cs
@@ -9,13 +9,15 @@ public static class SkipPatches
     [HarmonyPatch(typeof(VideoSequence), nameof(VideoSequence.Play))]
     public static bool VideoSequence_Play(ref VideoSequence __instance)
     {
-        return false;
+        return !Plugin.SkipIntros.Value;
     }
 
     [HarmonyPrefix]
     [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.ShowGamepadGUI))]
     public static bool MainMenuPanel_ShowGamepadGUI(ref MainMenuPanel __instance)
     {
+        if (!Plugin.SkipIntros.Value) return true;
+        
         __instance.CloseGamepadPanel();
         return false;
     }
@@ -24,6 +26,8 @@ public static class SkipPatches
     [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.PlayPressKeyReminderAnimationDelayed))]
     public static void MainMenuPanel_PlayPressKeyReminderAnimationDelayed(ref MainMenuPanel __instance)
     {
+        if (!Plugin.SkipIntros.Value) return;
+        
         __instance.PlayOpenAnimation();
         __instance.StopPressKeyReminderAnimation();
         __instance.SetInteractable(true, __instance.mainMenuButtons);
@@ -33,6 +37,8 @@ public static class SkipPatches
     [HarmonyPatch(typeof(MainMenuPanel), nameof(MainMenuPanel.CheckContinueAndSelectFirstButton))]
     public static void MainMenuPanel_CheckContinueAndSelectFirstButton(ref MainMenuPanel __instance)
     {
+        if (!Plugin.LoadStraightIntoGame.Value) return;
+        
         if (__instance.HasLoadedGame)
         {
             __instance.StartGame();

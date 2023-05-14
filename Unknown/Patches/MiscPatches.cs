2ffa9ab69bd9157441a9e809ecad3471fa9484dc
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-14 19:44:04 +1000

    Add UTF-8 to MiscPatches.cs and patch SetArmorAnimatorInteger method.

---
+++ Unknown/Patches/MiscPatches.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using HarmonyLib;
+
+namespace Unknown.Patches;
+
+[HarmonyPatch]
+public static class MiscPatches
+{
+
+    [HarmonyWrapSafe]
+    [HarmonyFinalizer]
+    [HarmonyPatch(typeof(HeroMerchantController), nameof(HeroMerchantController.SetArmorAnimatorInteger), typeof(string), typeof(int))]
+    public static Exception Finalizer()
+    {
+      return null;
+    }
+}
